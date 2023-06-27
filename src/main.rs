22520434e97bec30302e93019efa401e18d96b9d
Christopher <chris@grislygrotto.nz>
2023-06-27 20:35:06 +1200

    partial work on calculation

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ use iced::{Sandbox, widget::{column, text, container, row, button}, Settings, Le
 #[derive(Debug, Clone)]
 enum CalcMessage {
     Number(u32),
-    OpMul, OpDiv, OpAdd, OpSub,
+    Op(CalcOp),
     Negate, Fraction,
     Eval
 }
 
+#[derive(Debug, Clone)]
 enum CalcOp {
     Add, Sub, Mul, Div
 }
 
 struct CalcState {
-    displayed: f64,
+    displayed: String,
     current_op: Option<CalcOp>,
+    op_just_selected: bool,
     last_entered: Option<f64>
 }
 
@@ -23,16 +25,52 @@ impl Sandbox for CalcState {
 
     fn new() -> Self {
         Self {
-            displayed: 0.,
+            displayed: "0".into(),
             current_op: None,
+            op_just_selected: false,
             last_entered: None
         }
     }
 
     fn title(&self) -> String { "Iced Calc".into() }
 
-    fn update(&mut self, _message: Self::Message) {
-        
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            CalcMessage::Op(op) => { 
+                self.current_op = Some(op);
+                self.op_just_selected = true;
+            },
+            CalcMessage::Number(n) => {
+                if self.displayed == "0" || self.op_just_selected {
+                    if self.op_just_selected {
+                        self.last_entered = Some(self.displayed.parse().unwrap());
+                    }
+                    self.displayed = format!("{n}");
+                } else {
+                    self.displayed = format!("{}{n}", self.displayed);
+                }
+                self.op_just_selected = false;
+            },
+            CalcMessage::Negate => {
+                if self.displayed.starts_with("-") {
+                    self.displayed = self.displayed[1..].to_owned();
+                } else {
+                    self.displayed = format!("-{}", self.displayed);
+                }
+            },
+            CalcMessage::Fraction => {
+                if !self.displayed.contains(".") {
+                    self.displayed = format!("{}.", self.displayed);
+                }
+            },
+            CalcMessage::Eval => {
+                if self.current_op.is_none() {
+                    return;
+                }
+                let op = self.current_op.as_ref().unwrap();
+                let number: f64 = self.displayed.parse().unwrap();
+            }
+        }    
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
@@ -45,25 +83,25 @@ impl Sandbox for CalcState {
                     button("7").on_press(CalcMessage::Number(7)),
                     button("8").on_press(CalcMessage::Number(8)),
                     button("9").on_press(CalcMessage::Number(9)),
-                    button("÷").on_press(CalcMessage::OpDiv),
+                    button("÷").on_press(CalcMessage::Op(CalcOp::Div)),
                 ],
                 row![
                     button("4").on_press(CalcMessage::Number(4)),
                     button("5").on_press(CalcMessage::Number(5)),
                     button("6").on_press(CalcMessage::Number(6)),
-                    button("×").on_press(CalcMessage::OpMul),
+                    button("×").on_press(CalcMessage::Op(CalcOp::Mul)),
                 ],
                 row![
                     button("1").on_press(CalcMessage::Number(1)),
                     button("2").on_press(CalcMessage::Number(2)),
                     button("3").on_press(CalcMessage::Number(3)),
-                    button("+").on_press(CalcMessage::OpAdd),
+                    button("+").on_press(CalcMessage::Op(CalcOp::Add)),
                 ],
                 row![
                     button("±").on_press(CalcMessage::Negate),
                     button("0").on_press(CalcMessage::Number(0)),
                     button(".").on_press(CalcMessage::Fraction),
-                    button("-").on_press(CalcMessage::OpSub),
+                    button("-").on_press(CalcMessage::Op(CalcOp::Sub)),
                 ],
                 button("=").on_press(CalcMessage::Eval),
             ];
