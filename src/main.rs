bc84865b6e1f25ea9c18045410b76a31d1cbec6c
Christopher <chris@grislygrotto.nz>
2023-06-27 14:17:05 +1200

    adding some buttons

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use iced::{Sandbox, widget::{column, text, container}, Settings, Length, alignment::{Horizontal, Vertical}};
+use iced::{Sandbox, widget::{column, text, container, row, button}, Settings, Length, alignment::{Horizontal, Vertical}};
 
-#[derive(Debug)]
-enum CalcMessage {}
+#[derive(Debug, Clone)]
+enum CalcMessage {
+    Number(u32),
+    OpMul, OpDiv, OpAdd, OpSub,
+    Negate, Fraction,
+    Eval
+}
 
 enum CalcOp {
     Add, Sub, Mul, Div
@@ -31,17 +36,38 @@ impl Sandbox for CalcState {
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
+
         let content = 
-            column!(
+            column![
                 text("formula"),
                 text("displayed"),
-                text("row 1"),
-                text("row 2"),
-                text("row 3"),
-                text("row 4"),
-                text("equals"),
-            );
-            
+                row![
+                    button("7").on_press(CalcMessage::Number(7)),
+                    button("8").on_press(CalcMessage::Number(8)),
+                    button("9").on_press(CalcMessage::Number(9)),
+                    button("÷").on_press(CalcMessage::OpDiv),
+                ],
+                row![
+                    button("4").on_press(CalcMessage::Number(4)),
+                    button("5").on_press(CalcMessage::Number(5)),
+                    button("6").on_press(CalcMessage::Number(6)),
+                    button("×").on_press(CalcMessage::OpMul),
+                ],
+                row![
+                    button("1").on_press(CalcMessage::Number(1)),
+                    button("2").on_press(CalcMessage::Number(2)),
+                    button("3").on_press(CalcMessage::Number(3)),
+                    button("+").on_press(CalcMessage::OpAdd),
+                ],
+                row![
+                    button("±").on_press(CalcMessage::Negate),
+                    button("0").on_press(CalcMessage::Number(0)),
+                    button(".").on_press(CalcMessage::Fraction),
+                    button("-").on_press(CalcMessage::OpSub),
+                ],
+                button("=").on_press(CalcMessage::Eval),
+            ];
+
         container(content)
             .width(Length::Fill)
             .height(Length::Fill)
