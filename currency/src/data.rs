bce7fdc0f7140b9697319a9a9772dfe1d56cf7a9
Christopher (mite) <chris@grislygrotto.nz>
2023-07-06 17:40:51 +1200

    swapped out scraping implementation with just a random rate calc.

--- currency/src/data.rs
+++ currency/src/data.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+use rand::Rng;
 
 pub const CURRENCIES: [&str; 5] = [
     "NZD",
@@ -7,23 +9,9 @@ pub const CURRENCIES: [&str; 5] = [
     "JPY",
 ];
 
-pub async fn calculate(value: f64, from_denom: String, to_denom: String) -> String {
-    let url = format!("https://www.google.com/finance/quote/{from_denom}-{to_denom}");
-
-    let client = reqwest::Client::new();
-    let response = client
-        .get(url)
-        .header("Host", "www.google.com")
-        .send()
-        .await
-        .unwrap();
-
-    let body = response.text().await.unwrap();
-    let token = "<div class=\"YMlKec fxKbKc\">";
-    let index_1 = body.find(token).unwrap() + token.len();
-    let index_2 = body[index_1..].find("</div>").unwrap();
-    let rate = &body[index_1..(index_2 + index_1)];
-    let rate = rate.parse::<f64>().unwrap();
-    let value = rate * value;
-    format!("{:.2}", value)
+pub async fn calculate(value: f64, _from_denom: String, _to_denom: String) -> String {
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    let mut rng = rand::thread_rng();
+    let rate = rng.gen_range(0.1..2.);
+    format!("{:.2}", value * rate)
 }
