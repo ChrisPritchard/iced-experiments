2967fb576b38852df3da831581cd5b7640ca40d7
Christopher <chris@grislygrotto.nz>
2023-07-26 19:23:56 +1200

    finished off this example

--- layers/src/main.rs
+++ layers/src/main.rs
@@ -1,8 +1,6 @@
-use element_wrapper::ElementWrapper;
 
-use iced::{Sandbox, widget::{text, button, container}, Settings, Length, Rectangle, Point, Size, Element, Background, Color};
+use iced::{Sandbox, widget::{text, button, container, text_input}, Settings, Length, Rectangle, Point, Size, Element, Background, Color, advanced::widget::Operation};
 use iced::widget::column;
-use layer::Layer;
 
 use crate::layer_manager::LayerManager;
 
@@ -57,37 +55,35 @@ impl Sandbox for LayersApp {
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
 
-        fn button_test(label: &str, on_pressed: &str) -> Element<'static, Message> {
-            button(text(label.to_string()).size(30)).on_press(Message::SetMessage(on_pressed.to_string())).into()
-        }
-
         fn rect(x: f32, y: f32, width: f32, height: f32) -> Rectangle<f32> {
             Rectangle::new(Point::new(x, y), Size::new(width, height))
         }
 
         column![
-            ElementWrapper::new(button_test("wrapper", "wrapper pressed")),
-            ElementWrapper::new(button_test("wrapper", "wrapper pressed")),
-            ElementWrapper::new(button_test("wrapper", "wrapper pressed")),
-            ElementWrapper::new(button_test("wrapper", "wrapper pressed")),
-            Layer::new(
-                rect(10., 10., 300., 300.), 
-                button_test("simple layer", "simple layer pressed")),
-            Layer::new(
-                rect(20., 20., 300., 300.), 
-                border(button_test("panel 1", "panel 1 pressed")).into()),
             LayerManager::new(vec![
                 layer_manager::Layer::new(
                     rect(200., 200., 300., 300.), 
-                    button_test("manager layer 1", "manager layer 1 pressed")
+                    border(column![
+                        text("some sample text"),
+                        text("on more than one line"),
+                        text("to see how it looks"),
+                    ].into()).into()
                 ),
                 layer_manager::Layer::new(
                     rect(220., 220., 300., 300.), 
-                    button_test("manager layer 2", "manager layer 2 pressed")
+                    border(column![
+                        text("some text and inputs"),
+                        text_input("placeholder", &self.message),
+                        button("button").on_press(Message::SetMessage("layer 2 pressed".into()))
+                    ].into()).into()
                 ),
                 layer_manager::Layer::new(
                     rect(240., 240., 300., 300.), 
-                    button_test("manager layer 3", "manager layer 3 pressed")
+                    border(column![
+                        text("final layer with"),
+                        text("yet another button"),
+                        button("button").on_press(Message::SetMessage("layer 3 pressed".into()))
+                    ].into()).into()
                 ),
             ]),
             text(&self.message)
